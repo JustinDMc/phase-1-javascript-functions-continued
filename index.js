0adbcf2d8d7617d20ce3ea8ef49a83fae6833626
Justin <jdanielmcarthur@msn.com>
2021-05-23 15:10:24 -0700

    commit tree 7988872f4bba2191b7e2eae9ad55336ae0fc4f3a

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = (activity = "go to the office") => {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(input = "*") {
+    return function (input2 = "special") {
+        return `You are ${input}${input2}${input}!`
+    }
+}
